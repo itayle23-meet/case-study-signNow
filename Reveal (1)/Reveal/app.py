7729d60fe48ecc483e15d65b7109b39af7c1792c
itayle23-meet <109908182+itayle23-meet@users.noreply.github.com>
2022-08-08 15:35:56 +0300

    Update app.py

--- Reveal (1)/Reveal/app.py
+++ Reveal (1)/Reveal/app.py
@@ -1,13 +1,21 @@
-rom flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
 config = {
-
-
-
+"apiKey": "AIzaSyDr67EonBHtLJzJIbjz22BA9735oa49tFI",
+"authDomain": "signnow-9d696.firebaseapp.com",
+"projectId": "signnow-9d696",
+"storageBucket": "signnow-9d696.appspot.com",
+"messagingSenderId": "816100294913",
+"appId": "1:816100294913:web:bd12d0a1a6e3d24757db00",
+"measurementId": "G-LBZHTSE7CY",
+"databaseURL": "https://signnow-9d696-default-rtdb.europe-west1.firebasedatabase.app/"
 }
-pp = Flask(__name__, template_folder='', static_folder='')
+
+
+
+app = Flask(__name__, template_folder='', static_folder='')
 app.config['SECRET_KEY'] = 'super-secret-key'
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
@@ -15,4 +23,9 @@ db = firebase.database()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template('index.htmal')
+	return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
